5a23e7478f5dabae64a68b84b04bc095ec41dce9
Winfred Huang <huang.winfred21@gmail.com>
2022-01-15 22:58:25 -0800

    Add validation for user model

---
+++ wp_backend/app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+end
