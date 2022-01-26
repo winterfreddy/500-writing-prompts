5d28f6238778f4981e7f50b74760183dfabefee9
Winfred Huang <huang.winfred21@gmail.com>
2022-01-25 22:18:46 -0800

    Add sessions controller

---
+++ wp_backend/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+
+    def new 
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user
+            login_user(@user)
+            redirect_to api_user_url(@user)
+        else
+            render json: ["Incorrect email/password combination"], status: 422
+        end
+    end
+
+    def destroy
+        if current_user
+            logout_user
+            render json: {}
+        else
+            render json: ["No current user"], status: 422
+        end
+    end
+
+end
