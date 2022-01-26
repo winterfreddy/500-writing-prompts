8db6f7e32bf9beac8af2ed0f323176fd7da85f9c
Winfred Huang <huang.winfred21@gmail.com>
2022-01-25 22:29:46 -0800

    Update responses controller

--- wp_backend/app/controllers/api/responses_controller.rb
+++ wp_backend/app/controllers/api/responses_controller.rb
@@ -1,2 +1,52 @@
 class Api::ResponsesController < ApplicationController
+    before_action :require_user, only: [:create, :update, :destroy]
+
+    def index
+        @responses = Response.all
+        render :index
+    end
+
+    def show
+        @response = Response.find_by(id: params[:id])
+        if @response
+            render :show
+        else
+            render json: ['Fetching response failed: That response does not exist'], status: 422
+        end
+    end
+
+    def create
+        @response = Response.new(response_params)
+        if @response.save!
+            render :show
+        else
+            render json: @response.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @response = Response.find_by(id: params[:id])
+        if @response && @response.update!(response_params)
+            render :show
+        elsif !@response
+            render json: ["Updating response failed: That response does not exist"], status: 422
+        else
+            render json: @response.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @response = Response.find_by(id: params[:id])
+        if @response
+            @response.destroy
+        else
+            render json: ["Deleting response failed: That response does not exist"], status: 422
+        end
+    end
+
+    private
+    def response_params
+        self.params.require(:response).permit(:body, :prompt_id, :author_id)
+    end
+    
 end
