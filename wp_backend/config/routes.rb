65a0c1088fbca1db7a7d1e9dd6a7b3efbf733d9f
Winfred Huang <huang.winfred21@gmail.com>
2022-01-25 22:15:27 -0800

    Add routes

--- wp_backend/config/routes.rb
+++ wp_backend/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index, :create, :show]
+    resource :session, only: [:new, :create, :destroy]
+    resources :prompts, only: [:index, :show, :create, :destroy, :update]
+    resources :responses, only: [:index, :show, :create, :destroy, :update]
+  end
+  
 end
