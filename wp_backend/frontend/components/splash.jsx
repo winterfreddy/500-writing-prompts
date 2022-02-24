b7448df6f85c4bd71b90f6f834111e022c2f533e
Winfred Huang <huang.winfred21@gmail.com>
2022-02-23 23:55:57 -0800

    Organize text on splash page

--- wp_backend/frontend/components/splash.jsx
+++ wp_backend/frontend/components/splash.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const SplashPage = () => {
     return (
-        <div>
-            <label>500+ Writing Prompts</label>
-            <div>
+        <div className="splashpage-container">
+            <label className="splashpage-title">500+ Writing Prompts</label>
+            <div className="splashpage-subcontainer">
                 <img src="images/undraw_Diary_re_4jpc.png" alt="woman standing to a life-sized diary page"/>
-                <div>
+                <div className="splashpage-subtext">
                     <label>Pick a prompt</label>
                     <label>Write a response</label>
                     <label>Share with the world</label>
