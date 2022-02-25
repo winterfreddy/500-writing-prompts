aca27a814bbedc7c0171c2e003902052039fb556
Winfred Huang <huang.winfred21@gmail.com>
2022-02-24 21:32:11 -0800

    Adjust title font

--- wp_backend/frontend/components/splash.jsx
+++ wp_backend/frontend/components/splash.jsx
@@ -7,9 +7,9 @@ const SplashPage = () => {
             <div className="splashpage-subcontainer">
                 <img className="splashpage-image" src="images/undraw_Diary_re_4jpc.png" alt="woman standing to a life-sized diary page"/>
                 <div className="splashpage-subtext">
-                    <label>Pick a prompt</label>
-                    <label>Write a response</label>
-                    <label>Share with the world</label>
+                    <label className="splashpage-subtext-label">Pick <label>a prompt</label></label>
+                    <label className="splashpage-subtext-label">Write <label>a response</label></label>
+                    <label className="splashpage-subtext-label">Share <label>with the world</label></label>
                 </div>
             </div>
         </div>
